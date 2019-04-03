f9b61fd8ecfa85c1fe0c7f6a0667423aa903522f
JoseNA <josenava@estudiantec.cr>
2019-04-02 23:38:03 -0600

    comentarios al código

--- codificacion/requerimiento_0.py
+++ codificacion/requerimiento_0.py
@@ -3,12 +3,12 @@
 
 def R0(fecha_es_tupla):
 	if (len(fecha_es_tupla) == 3): 					# Verificar que la longitud de la tupla sea la correcta
-		for i in range(0, 3):						# Iterar en un rango de 0 a 2 sobre la tupla para accesar los valores
-			if (type(fecha_es_tupla[i]) is int):	# Comprobar que los elementos de la tupla sean números enteros
-				if (fecha_es_tupla[i] < 1):			# Comprobar que los elementos de la tupla sean números positivos
+		for i in range(0, 3):									# Iterar en un rango de 0 a 2 sobre la tupla para accesar los valores
+			if (type(fecha_es_tupla[i]) is int):# Comprobar que los elementos de la tupla sean números enteros
+				if (fecha_es_tupla[i] < 1):				# Comprobar que los elementos de la tupla sean números positivos
 					return False
 			else:
 				return False
 	else:
 		return False
-	return True										# La tupla ingresada es válida
+	return True															# La tupla ingresada es válida
--- codificacion/requerimiento_1.py
+++ codificacion/requerimiento_1.py
@@ -2,10 +2,10 @@
 # El resultado debe ser un valor booleano, True o False
 
 def R1(bisiesto):
-	if (type(bisiesto) is int):																# Comprobar que el año ingresado sea número entero
+	if (type(bisiesto) is int):																														# Comprobar que el año ingresado sea número entero
 		if (((bisiesto % 4) == 0) and ((bisiesto % 100) != 0)) or ((bisiesto % 400) == 0):	# Un año es bisiesto si:
-			return True																		# .. es divisible entre 4 y no entre 100 
-		else:																				# .. o si es divisible entre 400
+			return True																																				# .. es divisible entre 4 y no entre 100 
+		else:																																								# .. o si es divisible entre 400
 			return False
 	else:
 		return False
