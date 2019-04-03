9b9cb96d1fe14384b7c84edb436a2db0d1f43518
JoseNA <josenava@estudiantec.cr>
2019-04-02 21:04:54 -0600

    commit R0

---
+++ codificacion/requerimiento_0.py
@@ -0,0 +1,2 @@
+# R0(fecha_es_tupla): Todas las fechasserán creadas como tuplasde tresnúmeros enterospositivos(ternas),
+# en este orden: (año, mes, día).El resultado debe ser un valor booleano, TrueoFalse.
