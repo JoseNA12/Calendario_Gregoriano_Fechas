7ba24db250f98e67e6477fc5ef2993f71dbf9e9e
JoseNA <josenava@estudiantec.cr>
2019-04-03 17:57:11 -0600

    comnetario agregado a R1

--- codificacion/requerimiento_1.py
+++ codificacion/requerimiento_1.py
@@ -11,7 +11,7 @@ def R1(bisiesto):
     if (type(bisiesto) is int):								   # Comprobar que el año ingresado sea número entero
         if (((bisiesto % 4) == 0) and ((bisiesto % 100) != 0)) or ((bisiesto % 400) == 0): # Un año es bisiesto si:
             return True									   # .. es divisible entre 4 y no entre 100
-        else:
+        else:                                                                              # .. es divisible entre 400
             return False
     else:
         return False
