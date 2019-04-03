597cbe933a4683be6485b16815ac5616ffdf040f
JoseNA <josenava@estudiantec.cr>
2019-04-02 22:13:31 -0600

    comentarios al requerimiento 1

--- codificacion/requerimiento_1.py
+++ codificacion/requerimiento_1.py
@@ -3,9 +3,9 @@
 
 def R1(bisiesto):
 	if (type(bisiesto) is int):																# Comprobar que el año ingresado sea número entero
-		if (((bisiesto % 4) == 0) and ((bisiesto % 100) != 0)) or ((bisiesto % 400) == 0):
-			return True
-		else:
+		if (((bisiesto % 4) == 0) and ((bisiesto % 100) != 0)) or ((bisiesto % 400) == 0):	# Un año es bisiesto si:
+			return True																		# .. es divisible entre 4 y no entre 100 
+		else:																				# .. o si es divisible entre 400
 			return False
 	else:
 		return False
---
+++ codificacion/requerimiento_2.py
@@ -0,0 +1,2 @@
+# R2(fecha_es_valida): Dada una fecha, determinar si ésta es válida.
+# El resultado debe ser un valor booleano, TrueoFalse.
