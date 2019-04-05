94142a39048054c779750d18aa88f0dfd658e22b
Josue Suarez <josuarez2297@gmail.com>
2019-04-04 21:17:53 -0600

    Upd R4

--- codificacion/requerimiento_4.py
+++ codificacion/requerimiento_4.py
@@ -3,29 +3,30 @@
 
 """
 Entradas: "fecha": Lista en forma de tupla con valores enteros positivos que representan una fecha del Calendario Gregoriano
-Salidas: Entero: Sumatoria de la cantidad de días transcurridos entre el 1 de Enero y la fecha indicada. 
+Salidas: Entero: Sumatoria de la cantidad de dias transcurridos entre el 1 de Enero y la fecha indicada. 
 Restricciones: El parametro de entrada tiene que ser una lista de 3 elementos, siendo estos numeros enteros positivos
 """
 
+import requerimiento_0
 import requerimiento_1
 import requerimiento_2
 
-def R4(fecha):
-    if (requerimiento_2.R2(fecha)):
-        anio = fecha[0]
-        mes = fecha[1]
-        dia = fecha[2]
-        dias_meses = [31,28,31,30,31,30,31,31,30,31,30,31] #Los días que posee cada mes en orden de izquierda a derecha
+def R4(dias_desde_primero_enero):
+    if ((requerimiento_0.R0(dias_desde_primero_enero)) and (requerimiento_2.R2(dias_desde_primero_enero))):
+        anio = dias_desde_primero_enero[0]
+        mes = dias_desde_primero_enero[1]
+        dia = dias_desde_primero_enero[2]
+        dias_meses = [31,28,31,30,31,30,31,31,30,31,30,31] #Los dias que posee cada mes en orden de izquierda a derecha
     
         cantidad_dias = 0
         
-        for i in range(mes- 1): #Sumar todos los días desde enero hasta mes anterior
+        for i in range(mes- 1): #Sumar todos los dias desde enero hasta mes anterior
             cantidad_dias += dias_meses[i]
             i += 1
             
-        cantidad_dias += dia - 1 #Sumarle la cantidad de días que han pasado completamente en el mes actual
+        cantidad_dias += dia - 1 #Sumarle la cantidad de dias que han pasado completamente en el mes actual
         
-        if(2 < mes and requerimiento_1.R1(anio)): #Si es año bisiesto y mes mayor a febrero
+        if(2 < mes and requerimiento_1.R1(anio)): #Si es anio bisiesto y mes mayor a febrero
             cantidad_dias += 1
         
         print(cantidad_dias)
@@ -33,8 +34,6 @@ def R4(fecha):
     else:
         print("Error en el formato de fecha indicado")
 
-    
-    
     
         
 
